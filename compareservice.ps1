8f18b7d1daac25d52a09cf270eddd5bc435c0834
OussGfr <34546656+OussGfr@users.noreply.github.com>
2019-02-25 11:00:32 +0100

    Update compareservice.ps1

--- compareservice.ps1
+++ compareservice.ps1
@@ -1,4 +1,4 @@
 $reference = Get-Service
-$difference = Get-Service -ComputerName a92sv037gcar1r
+$difference = Get-Service -ComputerName """""
 
 Compare-Object $own $other -property Name, Status -PassThru | Sort-Object DisplayName | Select MachineName, Status, DisplayName, Name | Format-Table -AutoSize
